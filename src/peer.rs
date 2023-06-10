5958cf0e44be30be512edce69c12e884bcb3d44c
gabrieldemian <gabrielgcr45@gmail.com>
2023-06-10 20:30:14 -0300

    peers.rs

--- src/peer.rs
+++ src/peer.rs
@@ -1,6 +1,25 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use futures::{SinkExt, StreamExt};
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    sync::Arc,
+    time::Duration,
+};
+use tokio::io::AsyncWriteExt;
+use tokio::{io::AsyncReadExt, select, sync::mpsc::Sender};
+use tokio_util::codec::Framed;
 
-use crate::{bitfield::Bitfield, tcp_wire::lib::BlockInfo};
+use log::{debug, info};
+use tokio::{net::TcpStream, time::interval};
+
+use crate::{
+    bitfield::Bitfield,
+    error::Error,
+    tcp_wire::{
+        lib::BlockInfo,
+        messages::{Handshake, Message, PeerCodec},
+    },
+    torrent::{Torrent, TorrentCtx, TorrentMsg},
+};
 
 #[derive(Debug, Clone)]
 pub struct Peer {
@@ -21,6 +40,7 @@ pub struct Peer {
     /// Requests that we'll send to this peer,
     /// once he unchoke us
     pub pending_requests: Vec<BlockInfo>,
+    pub torrent: Option<Arc<Torrent>>,
 }
 
 impl Default for Peer {
@@ -36,6 +56,7 @@ impl Default for Peer {
             addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 0),
             pending_requests: vec![],
             id: None,
+            torrent: None,
         }
     }
 }
@@ -51,7 +72,179 @@ impl From<SocketAddr> for Peer {
 }
 
 impl Peer {
-    fn new() -> Self {
-        Self::default()
+    pub fn new(torrent: Arc<Torrent>) -> Self {
+        let mut p = Self::default();
+        p.torrent = Some(torrent);
+        p
+    }
+    pub async fn run(
+        &mut self,
+        our_handshake: Handshake,
+        ctx: Arc<TorrentCtx>,
+        tx: Sender<TorrentMsg>,
+    ) -> Result<(), Error> {
+        let mut tick_timer = interval(Duration::from_secs(1));
+        let mut socket = TcpStream::connect(self.addr).await?;
+
+        // Send Handshake to peer
+        socket.write_all(&mut our_handshake.serialize()?).await?;
+
+        // Read Handshake from peer
+        let mut handshake_buf = [0u8; 68];
+        socket.read_exact(&mut handshake_buf).await?;
+        let their_handshake = Handshake::deserialize(&handshake_buf)?;
+
+        // Validate their handshake against ours
+        if !their_handshake.validate(&our_handshake) {
+            return Err(Error::HandshakeInvalid);
+        }
+
+        // Update peer_id that was received from
+        // their handshake
+        self.id = Some(their_handshake.peer_id);
+
+        let (mut sink, mut stream) = Framed::new(socket, PeerCodec).split();
+
+        // If there is a Bitfield message to be received,
+        // it will be the very first message after the handshake,
+        // receive it here, add this information to peer
+        // and create a new Bitfield for `Torrent` with the same length,
+        // but completely empty
+        let msg = stream.next().await;
+        if let Some(Ok(Message::Bitfield(bitfield))) = msg {
+            info!("Received Bitfield message from peer {:?}", self.addr);
+            info!("{:?}", bitfield);
+            // update the bitfield of the `Torrent`
+            // will create a new, empty bitfield, with
+            // the same len
+            tx.send(TorrentMsg::UpdateBitfield(bitfield.inner.len()))
+                .await
+                .unwrap();
+        }
+
+        // Send Interested & Unchoke to peer
+        // We want to send and receive blocks
+        // from everyone
+        sink.send(Message::Interested).await?;
+        self.am_interested = true;
+        sink.send(Message::Unchoke).await?;
+        self.am_choking = false;
+
+        loop {
+            select! {
+                _ = tick_timer.tick() => {
+                    debug!("tick peer {:?}", self.addr);
+                }
+                Some(msg) = stream.next() => {
+                    let msg = msg?;
+                    match msg {
+                        Message::KeepAlive => {
+                            info!("Peer {:?} sent Keepalive", self.addr);
+                        }
+                        Message::Bitfield(bitfield) => {
+                            info!("\t received bitfield");
+                            info!("{:?}", bitfield);
+
+                            let first = bitfield.into_iter().find(|x| x.bit == 1);
+
+                            if let Some(first) = first {
+                                info!("requesting first bit with index {:?}", first.index);
+                                let block = BlockInfo::new().index(first.index as u32);
+                                sink.send(Message::Request(block)).await?;
+                                // todo: I have to wait until Unchoke to send
+                                // most of the times, Bitfield and Have are immediately
+                                // followed by an Unchoke,
+                            }
+                        }
+                        Message::Unchoke => {
+                            self.peer_choking = false;
+                            info!("Peer {:?} unchoked us", self.addr);
+
+                            // the download flow (Request and Piece) msgs
+                            // will start when the peer Unchokes us
+                            // send the first request to peer here
+                            // - logic fn piece.request
+                            // loop
+                            // 1 - get next piece from bitfield
+                            // check:
+                            // if it has already been requested,
+                            // on the list: torrent.requested_pieces
+                            // false? go back to 1
+                            // true? update torrent.requested_pieces
+                            // send the Request
+
+                            if self.am_interested {
+                                let rp = ctx.requested_pieces.read().await;
+                                let piece = self.pieces.clone().zip(rp.iter()).find(|(p, r)| p.index != **r as usize);
+
+                                if let Some(piece) = piece {
+                                    println!("found a piece {:?}", piece.0);
+                                }
+                            }
+                        }
+                        Message::Choke => {
+                            self.peer_choking = true;
+                            self.pending_requests.clear();
+                            info!("Peer {:?} choked us", self);
+                            // clear any pending requests
+                        }
+                        Message::Interested => {
+                            self.peer_interested = true;
+                            info!("Peer {:?} is interested in us", self.addr);
+                            // peer will start to request blocks from us soon
+                        }
+                        Message::NotInterested => {
+                            self.peer_interested = false;
+                            info!("Peer {:?} is not interested in us", self.addr);
+                            // peer won't request blocks from us anymore
+                        }
+                        Message::Have(piece) => {
+                            debug!("Peer {:?} has a piece_index of {:?}", self.addr, piece);
+                            // Have is usually sent when I peer has downloaded
+                            // a new block, however, some peers, after handshake,
+                            // send an incomplete bitfield followed by a sequence of
+                            // have's. They do this to try to prevent censhorship
+                            // from ISPs. This is one of the reasons why
+                            // this client uses the Bitfield as a source of truth
+                            // to manage pieces. For each Have message, we will
+                            // overwrite the piece_index on the peer bitfield.
+                            self.pieces.set(piece as usize);
+                            let block = BlockInfo::new().index(piece as u32);
+                            // this will be sent, as a request, on Unchoke
+                            self.pending_requests.push(block);
+                        }
+                        Message::Piece(block) => {
+                            // validate block,
+                            // if we requested it,
+                            // if it has 16KiB,
+                            // if the hash is valid,
+                            // false? remove from torrent.requested_blocks
+                            // true? update our bitfield
+                            // send msg to `Disk` tx
+                            // Advertise to the peers that
+                            // doesn't have this piece, that
+                            // we Have it.
+                            // Request another piece
+                            // call fn piece.request from Unchoke logic
+                            // ping pong of request & piece will start
+                        }
+                        Message::Cancel(block_info) => {
+                            info!("Peer {:?} canceled a block {:?}", self.addr, block_info);
+                        }
+                        Message::Request(block_info) => {
+                            info!("Peer {:?} request a block {:?}", self.addr, block_info);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn a() {
+        //
     }
 }
--- src/torrent.rs
+++ src/torrent.rs
@@ -2,30 +2,19 @@ use crate::bitfield::Bitfield;
 use crate::error::Error;
 use crate::magnet_parser::get_info_hash;
 use crate::peer::Peer;
-use crate::tcp_wire::lib::BlockInfo;
 use crate::tcp_wire::messages::Handshake;
-use crate::tcp_wire::messages::Message;
-use crate::tcp_wire::messages::PeerCodec;
 use crate::tracker::tracker::Tracker;
-use futures::stream::SplitSink;
-use futures::{SinkExt, StreamExt};
 use log::debug;
 use log::info;
 use magnet_url::Magnet;
-use std::collections::BTreeMap;
 use std::sync::Arc;
-use std::sync::Mutex;
 use std::time::Duration;
-use tokio::io::AsyncReadExt;
-use tokio::io::AsyncWriteExt;
-use tokio::net::TcpStream;
-use tokio::select;
 use tokio::spawn;
 use tokio::sync::mpsc::Receiver;
 use tokio::sync::mpsc::Sender;
+use tokio::sync::RwLock;
 use tokio::time::interval;
 use tokio::time::Interval;
-use tokio_util::codec::Framed;
 
 #[derive(Debug)]
 pub enum TorrentMsg {
@@ -35,29 +24,41 @@ pub enum TorrentMsg {
     // once a peer send the first bitfield message,
     // we will update the torrent bitfield
     UpdateBitfield(usize),
-    Request(Peer, SplitSink<Framed<TcpStream, PeerCodec>, Message>),
 }
 
+/// This is the main entity responsible for the high-level management of
+/// a torrent download or upload.
 #[derive(Debug)]
 pub struct Torrent {
+    pub ctx: Arc<TorrentCtx>,
     pub bitfield: Bitfield,
     pub tx: Sender<TorrentMsg>,
     pub rx: Receiver<TorrentMsg>,
     pub tick_interval: Interval,
     pub in_end_game: bool,
-    // todo: how to make this global to all peers?
-    pub requested_pieces: Arc<Mutex<BTreeMap<u32, bool>>>,
+}
+
+/// Information and methods shared with peer sessions in the torrent.
+/// This type contains fields that need to be read or updated by peer sessions.
+/// Fields expected to be mutated are thus secured for inter-task access with
+/// various synchronization primitives.
+#[derive(Debug)]
+pub struct TorrentCtx {
+    pub requested_pieces: Arc<RwLock<Vec<u32>>>,
 }
 
 impl Torrent {
     pub async fn new(tx: Sender<TorrentMsg>, rx: Receiver<TorrentMsg>) -> Self {
-        let requested_pieces = Arc::new(Mutex::new(BTreeMap::default()));
+        let requested_pieces = Arc::new(RwLock::new(Vec::new()));
+
+        let ctx = Arc::new(TorrentCtx { requested_pieces });
+
         Self {
+            ctx,
             bitfield: Bitfield::default(),
             in_end_game: false,
             tx,
             rx,
-            requested_pieces,
             tick_interval: interval(Duration::new(1, 0)),
         }
     }
@@ -80,172 +81,6 @@ impl Torrent {
                         let inner = vec![0_u8; len];
                         self.bitfield = Bitfield::from(inner);
                     }
-                    TorrentMsg::Request(peer, sink) => {
-                        //
-                    }
-                }
-            }
-        }
-    }
-
-    #[tracing::instrument]
-    pub async fn listen_to_peer(
-        tx: Sender<TorrentMsg>,
-        mut peer: Peer,
-        our_handshake: Handshake,
-    ) -> Result<(), Error> {
-        let mut tick_timer = interval(Duration::from_secs(1));
-        let mut socket = TcpStream::connect(peer.addr).await?;
-
-        // Send Handshake to peer
-        socket.write_all(&mut our_handshake.serialize()?).await?;
-
-        // Read Handshake from peer
-        let mut handshake_buf = [0u8; 68];
-        socket.read_exact(&mut handshake_buf).await?;
-        let their_handshake = Handshake::deserialize(&handshake_buf)?;
-
-        // Validate their handshake against ours
-        if !their_handshake.validate(&our_handshake) {
-            return Err(Error::HandshakeInvalid);
-        }
-
-        // Update peer_id that was received from
-        // their handshake
-        peer.id = Some(their_handshake.peer_id);
-
-        let (mut sink, mut stream) = Framed::new(socket, PeerCodec).split();
-
-        // If there is a Bitfield message to be received,
-        // it will be the very first message after the handshake,
-        // receive it here, add this information to peer
-        // and create a new Bitfield for `Torrent` with the same length,
-        // but completely empty
-        let msg = stream.next().await;
-        if let Some(Ok(Message::Bitfield(bitfield))) = msg {
-            info!("Received Bitfield message from peer {:?}", peer);
-            info!("{:?}", bitfield);
-            // update the bitfield of the `Torrent`
-            // will create a new, empty bitfield, with
-            // the same len
-            tx.send(TorrentMsg::UpdateBitfield(bitfield.inner.len()))
-                .await
-                .unwrap();
-        }
-
-        // Send Interested & Unchoke to peer
-        // We want to send and receive blocks
-        // from everyone
-        sink.send(Message::Interested).await?;
-        peer.am_interested = true;
-        sink.send(Message::Unchoke).await?;
-        peer.am_choking = false;
-
-        loop {
-            select! {
-                _ = tick_timer.tick() => {
-                    debug!("tick peer {:?}", peer);
-                }
-                Some(msg) = stream.next() => {
-                    let msg = msg?;
-                    match msg {
-                        Message::KeepAlive => {
-                            info!("Peer {:?} sent Keepalive", peer);
-                        }
-                        Message::Bitfield(bitfield) => {
-                            info!("\t received bitfield");
-                            info!("{:?}", bitfield);
-
-                            let first = bitfield.into_iter().find(|x| x.bit == 1);
-
-                            if let Some(first) = first {
-                                info!("requesting first bit with index {:?}", first.index);
-                                let block = BlockInfo::new().index(first.index as u32);
-                                sink.send(Message::Request(block)).await?;
-                                // todo: I have to wait until Unchoke to send
-                                // most of the times, Bitfield and Have are immediately
-                                // followed by an Unchoke,
-                            }
-                        }
-                        Message::Unchoke => {
-                            peer.peer_choking = false;
-                            info!("Peer {:?} unchoked us", peer);
-
-                            // the download flow (Request and Piece) msgs
-                            // will start when the peer Unchokes us
-                            // send the first request to peer here
-                            // - logic fn piece.request
-                            // loop
-                            // 1 - get next piece from bitfield
-                            // check:
-                            // if it has already been requested,
-                            // on the list: torrent.requested_pieces
-                            // false? go back to 1
-                            // true? update torrent.requested_pieces
-                            // send the Request
-
-                            if peer.am_interested {
-                                // tx.send(TorrentMsg::Request(peer.clone(), &mut sink)).await;
-                                // let block = BlockInfo::new().index(0);
-                                // sink.send(Message::Request(block)).await?;
-                            }
-                        }
-                        Message::Choke => {
-                            peer.peer_choking = true;
-                            info!("Peer {:?} choked us", peer);
-                            // clear any pending requests
-                        }
-                        Message::Interested => {
-                            peer.peer_interested = true;
-                            info!("Peer {:?} is interested in us", peer);
-                            // peer will start to request blocks from us soon
-                        }
-                        Message::NotInterested => {
-                            peer.peer_interested = false;
-                            info!("Peer {:?} is not interested in us", peer);
-                            // peer won't request blocks from us anymore
-                        }
-                        Message::Have(piece_index) => {
-                            debug!("Peer {:?} has a piece_index of {:?}", peer, piece_index);
-                            // Have is usually sent when I peer has downloaded
-                            // a new block, however, some peers, after handshake,
-                            // send an incomplete bitfield followed by a sequence of
-                            // have's. They do this to try to prevent censhorship
-                            // from ISPs. This is one of the reasons why
-                            // this client uses the Bitfield as a source of truth
-                            // to manage pieces. For each Have message, we will
-                            // overwrite the piece_index on the peer bitfield.
-
-                            peer.pieces.set(piece_index);
-
-                            let block = BlockInfo::new().index(piece_index as u32);
-                            // this will be sent, as a request, on Unchoke
-                            peer.pending_requests.push(block);
-                        }
-                        Message::Piece(block) => {
-                            info!("Peer {:?} sent a Piece of index {}", peer, block.index);
-                            info!("Block has {:?} KiB", block.block.len() / 1000);
-                            // validate block,
-                            // if we requested it,
-                            // if it has 16KiB,
-                            // if the hash is valid,
-                            // false? remove from torrent.requested_blocks
-                            // true? update our bitfield
-                            // send msg to `Disk` tx
-                            // Advertise to the peers that
-                            // doesn't have this piece, that
-                            // we Have it.
-                            // Request another piece
-                            // call fn piece.request from Unchoke logic
-                            // ping pong of request & piece will start
-                        }
-                        Message::Cancel(block_info) => {
-                            info!("Peer {:?} canceled a block {:?}", peer, block_info);
-                        }
-                        Message::Request(block_info) => {
-                            info!("Peer {:?} request a block {:?}", peer, block_info);
-                        }
-                    }
                 }
             }
         }
@@ -257,14 +92,15 @@ impl Torrent {
         peers: Vec<Peer>,
         our_handshake: Handshake,
     ) -> Result<(), Error> {
-        for peer in peers {
+        for mut peer in peers {
             let tx = self.tx.clone();
+            let ctx = Arc::clone(&self.ctx);
             let our_handshake = our_handshake.clone();
 
             debug!("listening to peer...");
 
             spawn(async move {
-                Self::listen_to_peer(tx, peer, our_handshake).await?;
+                peer.run(our_handshake, ctx, tx).await?;
                 Ok::<_, Error>(())
             });
         }
